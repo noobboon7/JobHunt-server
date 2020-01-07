eb6277a9dc0a9b12728c298e8b87c03008b0e295
Jerry V <jvr572@gmail.com>
2020-01-07 18:42:27 -0500

    api almost set

--- worker/tasks/fetch-github.js
+++ worker/tasks/fetch-github.js
@@ -1,6 +1,36 @@
 var fetch = require('node-fetch')
+var redis = require("redis"),
+    client = redis.createClient();
 
+const {promisify} = require('util');
+// const getAsync = promisify(client.get).bind(client);
+const setAsync = promisify(client.set).bind(client);
 
-export default async function fetchGithub(){
-  const jobs = await fetch()
+const baseURL = 'https://jobs.github.com/positions.json?'
+
+async function fetchGithub(){
+  let resultCount = 1,
+      onPage = 0; 
+  const allJobs = [];
+
+  while(resultCount > 0){
+    const res = await fetch(`${baseURL}page=${onPage}`);
+    const jobs = await res.json();
+
+    allJobs.push(...jobs);
+    resultCount = jobs.length;
+
+    console.log('got', jobs.length, 'jobs');
+    onPage++;
+  }
+
+  console.log('got', allJobs.length, 'jobs');
+  // sets github to redis 
+  const success = await setAsync('github', JSON.stringify(allJobs));
+
+  console.log({success});
 }
+
+fetchGithub()
+
+module.exports = fetchGithub;
