c316a179b9a3834bfb37a46dee4e5632f4cfc499
Jerry V <jvr572@gmail.com>
2020-01-08 23:58:16 -0500

    api

--- worker/tasks/fetch-github.js
+++ worker/tasks/fetch-github.js
@@ -3,10 +3,9 @@ var redis = require("redis"),
     client = redis.createClient();
 
 const {promisify} = require('util');
-// const getAsync = promisify(client.get).bind(client);
 const setAsync = promisify(client.set).bind(client);
 
-const baseURL = 'https://jobs.github.com/positions.json?'
+const baseURL = 'https://jobs.github.com/positions.json?';
 
 async function fetchGithub(){
   let resultCount = 1,
@@ -16,21 +15,40 @@ async function fetchGithub(){
   while(resultCount > 0){
     const res = await fetch(`${baseURL}page=${onPage}`);
     const jobs = await res.json();
-
+    
+    // spreads object into array
     allJobs.push(...jobs);
     resultCount = jobs.length;
 
     console.log('got', jobs.length, 'jobs');
     onPage++;
   }
-
   console.log('got', allJobs.length, 'jobs');
-  // sets github to redis 
-  const success = await setAsync('github', JSON.stringify(allJobs));
+
+  //  filter junior jobs 
+  const jrJobs = allJobs.filter(job => {
+    jobtitle = job.title.toLowerCase();
+    let isJunior = true;
+
+    // algo logic 
+    if(
+      jobtitle.includes('senior') ||
+      jobtitle.includes('manager') ||
+      jobtitle.includes('sr.') ||
+      jobtitle.includes('architect')
+    ) {
+      return false;
+    }
+    return true;
+  });
+  console.log('filtered down to', jrJobs.length);
+
+  // sets github jobs to redis 
+  const success = await setAsync('github', JSON.stringify(jrJobs));
 
   console.log({success});
 }
 
-fetchGithub()
+fetchGithub();
 
 module.exports = fetchGithub;
