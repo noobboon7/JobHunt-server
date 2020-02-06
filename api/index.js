29032cbd634487ba1daff662432e24869deac3f3
Jerry V <jvr572@gmail.com>
2020-02-06 13:33:08 -0500

    revert to prev

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ const getAsync = promisify(client.get).bind(client);
 app.get('/jobs', async (req, res) => {
   fetchGithub();
   const jobs = await getAsync('github');
-  res.header("Access-Control-Allow-Origin", "no-cors" );
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000/" );
   return res.send(jobs);
 });
 
